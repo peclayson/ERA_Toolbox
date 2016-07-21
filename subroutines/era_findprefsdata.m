678c2565688a84ba826afdc6412e051a4eef7372
Peter Clayson <peter.clayson@gmail.com>
2016-07-21 14:38:49 -0500

    new function: era_findprefsdata
    I got sick of writing the code to somersault through varargin for
    era_prefs and era_data. This function will do it for me.

---
+++ subroutines/era_findprefsdata.m
@@ -0,0 +1,55 @@
+function [era_prefs,era_data] = era_findprefsdata(varargin)
+%
+%Somersault through inputs to find era_prefs and era_data
+%
+%[era_prefs,era_data] = era_findprefsdata(varargin)
+%
+%Last Updated 7/21/16 
+%
+%Input
+% varargin - varargin from guis
+%
+%output
+% era_prefs - ERA Toolbox structure array containing preferences
+% era_data - ERA Toolbox structure array containing data
+%
+
+% Copyright (C) 2016 Peter E. Clayson
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program (gpl.txt). If not, see 
+%     <http://www.gnu.org/licenses/>.
+%
+
+%History 
+% by Peter Clayson (7/21/16)
+% peter.clayson@gmail.com
+%
+
+
+%Somersault through varargin inputs to check for which inputs were
+%defined and store those values. 
+%check if era_prefs has been defined
+ind = find(strcmp('era_prefs',varargin),1);
+if ~isempty(ind)
+    era_prefs = varargin{ind+1}; 
+end
+
+%check if era_data has been defined
+ind = find(strcmp('era_data',varargin),1);
+if ~isempty(ind)
+    era_data = varargin{ind+1}; 
+end
+
+end
+
